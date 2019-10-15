d1b3190df626b9f0679d0c2c330c02163046e848
Kevin Snijders <kevin_snijders@live.nl>
2019-10-15 08:59:51 +0200

    Updates

---
+++ precache-manifest.5f300238ea706093412cfbad3ec36ba3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0484b418b46168ac8855a83fc9ccdd6",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "cf09dbd0071c6d0840ee",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "9cb1003dda52787106ef",
+    "url": "/robofriends/static/css/main.8bfdc50c.chunk.css"
+  },
+  {
+    "revision": "cf09dbd0071c6d0840ee",
+    "url": "/robofriends/static/js/2.49eeba55.chunk.js"
+  },
+  {
+    "revision": "9cb1003dda52787106ef",
+    "url": "/robofriends/static/js/main.fe1f9333.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "b999676665db806c281797b2bbfca425",
+    "url": "/robofriends/static/media/background.b9996766.jpg"
+  },
+  {
+    "revision": "7581c48d0f9a1130080a7483b9461182",
+    "url": "/robofriends/static/media/loader.7581c48d.gif"
+  }
+]);
